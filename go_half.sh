ee7d3460fe154701309e54d87994f6dc0e0bcde6
root <root@surface.tomneilly.com>
2016-12-05 19:14:22 -0500

    Fixed stuff

--- go_half.sh
+++ go_half.sh
@@ -34,46 +34,21 @@ useradd twm18 -p `openssl passwd -crypt 123`;
 useradd astrozyk13 -p `openssl passwd -crypt 123`;
 useradd mea95 -p `openssl passwd -crypt 123`;
 
-echo "OS_USERNAME=admin" >> /etc/environment;
-echo "OS_PASSWORD=Panther$" >> /etc/environment;
-echo "OS_AUTH_URL=http://controller.cist.pitt.edu:5000/v2.0" >> /etc/environment;
-echo "OS_PROJECT_NAME=admin" >> /etc/environment;
-source /etc/environment;
-
-echo "OS_USERNAME=admin" >> /etc/profile;
-echo "OS_PASSWORD=Panther$" >> /etc/profile;
-echo "OS_AUTH_URL=http://controller.cist.pitt.edu:5000/v2.0" >> /etc/profile;
-echo "OS_PROJECT_NAME=admin" >> /etc/profile;
-source /etc/profile;
-
-OS_USERNAME=admin;
-OS_PASSWORD=Panther$;
-OS_AUTH_URL=http://controller.cist.pitt.edu:5000/v2.0;
-OS_PROJECT_NAME=admin;
-
-export OS_USERNAME=admin;
-export OS_PASSWORD=Panther$;
-export OS_AUTH_URL=http://controller.cist.pitt.edu:5000/v2.0;
-export OS_PROJECT_NAME=admin;
-
-cd /;
-git init;
-git clone https://github.com/neillyt/openstack.git;
-
 echo "136.142.139.140   controller      controller.cist.pitt.edu" >> /etc/hosts ;
 echo "136.142.139.141   network         network.cist.pitt.edu" >> /etc/hosts ;
 echo "136.142.139.142   compute         compute.cist.pitt.edu" >> /etc/hosts ;
 echo "136.142.139.143   compute2        compute2.cist.pitt.edu" >> /etc/hosts ;
 echo "136.142.139.144   compute3        compute3.cist.pitt.edu" >> /etc/hosts ;
 
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config;
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config;
+sed -i '/PermitRootLogin/a PermitRootLogin yes' sshd_config;
+sed -i '/PasswordAuthentication/a PasswordAuthentication yes' sshd_config
 
 wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm;
 wait;
 rpm -ivh epel-release-7-8.noarch.rpm;
 wait;
-
+yum -y install erlang;
+wait;
 yum -y upgrade;
 wait;
 
@@ -82,7 +57,6 @@ echo "**************************************************************************
 echo "***************************************************************************************";
 echo "***************************************************************************************";
 echo " ALL HOSTS: Disable selinux in /etc/selinux/config";
-echo " CONTROLLER: yum -y install erlang";
 echo " CONTROLLER: packstack --gen-answer-file /root/answers.txt";
 echo "***************************************************************************************";
 echo "***************************************************************************************";
