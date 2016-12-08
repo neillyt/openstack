50e4f7b73d29f7ddc293ef2f64df2bcda0f4ab34
root <root@hv1.learnwithlinux.com>
2016-12-07 22:03:42 -0500

    its smarter now

--- go_half.sh
+++ go_half.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "What is the hostname? Do not include '.cist.pitt.edu'";
+echo "What is the hostname? Do not include '.learnwithlinux.com'";
 echo "Good Example: awesomebox";
-echo "Bad Example: awesomebox.cist.pitt.edu";
+echo "Bad Example: awesomebox.learnwithlinux.com";
 read hostname;
 echo ""
 
@@ -22,33 +22,13 @@ wait;
 systemctl start chronyd;
 systemctl enable chronyd;
 
-echo "$hostname.cist.pitt.edu" > /etc/hostname;
+echo "$hostname.learnwithlinux.com" > /etc/hostname;
 
-useradd thn16 -p `openssl passwd -crypt 123`;
-useradd zsm5 -p `openssl passwd -crypt 123`;
-useradd lewicki -p `openssl passwd -crypt 123`;
-useradd sellison -p `openssl passwd -crypt 123`;
-useradd ellison -p `openssl passwd -crypt 123`;
-useradd dre15 -p `openssl passwd -crypt 123`;
-useradd twm18 -p `openssl passwd -crypt 123`;
-useradd astrozyk13 -p `openssl passwd -crypt 123`;
-useradd mea95 -p `openssl passwd -crypt 123`;
-
-sudo sed -i '/Allow root to run any commands anywhere/a thn16      ALL=(ALL)       ALL' /etc/sudoers;
-sudo sed -i '/Allow root to run any commands anywhere/a zsm5       ALL=(ALL)       ALL' /etc/sudoers;
-sudo sed -i '/Allow root to run any commands anywhere/a lewicki    ALL=(ALL)       ALL' /etc/sudoers;
-sudo sed -i '/Allow root to run any commands anywhere/a sellison   ALL=(ALL)       ALL' /etc/sudoers;
-sudo sed -i '/Allow root to run any commands anywhere/a ellison    ALL=(ALL)       ALL' /etc/sudoers;
-sudo sed -i '/Allow root to run any commands anywhere/a dre15      ALL=(ALL)       ALL' /etc/sudoers;
-sudo sed -i '/Allow root to run any commands anywhere/a twm18      ALL=(ALL)       ALL' /etc/sudoers;
-sudo sed -i '/Allow root to run any commands anywhere/a astrozyk13 ALL=(ALL)       ALL' /etc/sudoers;
-sudo sed -i '/Allow root to run any commands anywhere/a mea95      ALL=(ALL)       ALL' /etc/sudoers;
-
-echo "136.142.139.133   controller      controller.cist.pitt.edu" >> /etc/hosts ;
-echo "136.142.139.134   network         network.cist.pitt.edu" >> /etc/hosts ;
-echo "136.142.139.135   compute         compute.cist.pitt.edu" >> /etc/hosts ;
-echo "136.142.139.136   compute2        compute2.cist.pitt.edu" >> /etc/hosts ;
-echo "136.142.139.137   compute3        compute3.cist.pitt.edu" >> /etc/hosts ;
+echo "192.168.0.100   controller      controller.learnwithlinux.com" >> /etc/hosts ;
+echo "192.168.0.101   network         network.learnwithlinux.com" >> /etc/hosts ;
+echo "192.168.0.102   compute         compute.learnwithlinux.com" >> /etc/hosts ;
+echo "192.168.0.103   compute2        compute2.learnwithlinux.com" >> /etc/hosts ;
+echo "192.168.0.104   compute3        compute3.learnwithlinux.com" >> /etc/hosts ;
 
 sudo sed -i '/PermitRootLogin/a PermitRootLogin yes' /etc/ssh/sshd_config ;
 sudo sed -i '/PasswordAuthentication/a PasswordAuthentication yes' /etc/ssh/sshd_config ;
