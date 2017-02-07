3959d90efd655e3ad596995ea4013ab01df3d414
root <root@controller.learnwithlinux.com>
2017-02-07 17:43:56 -0500

    Controller node backup script

---
+++ controller_rsync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -e >> /var/log/backup.log
+echo "Backup for `hostname` started at `date`" >> /var/log/backup.log
+
+rsync -aAX --delete --exclude={"/var/log/","/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/","/media/","/srv/","/lost+found"} / hv1:/backups/controller_backup/
+
+STATUS=`echo $?`
+
+if [ "$STATUS" == "0" ]; then
+ echo "Backup for `hostname` completed SUCCESSFULLY at `date`" >> /var/log/backup.log
+else
+ echo "Backed for `hostname` FAILED at `date`" >> /var/log/backup.log
+fi
